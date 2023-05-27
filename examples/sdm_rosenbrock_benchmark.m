dbda98a8c785d1e5dfb7be30c148c3c3394e604a
yuenhern96@gmail.com <yuenhern.yu@gmail.com>
2023-05-27 15:07:37 +0800

    Add rosenbrock benchmark

--- examples/sdm_rosenbrock_benchmark.m
+++ examples/sdm_rosenbrock_benchmark.m
@@ -6,9 +6,9 @@ source("../src/solvers.m")
 source("../src/hessian_approx.m")
 
 % initialize symbol
-syms x;
+syms x y;
 % objection function
-f = x^2;
+f = 100*(y - x^2)^2 + (1-x)^2
 
 % derive gradient
 gradient_mat = gradient(f);
@@ -17,18 +17,18 @@ df = function_handle(gradient_mat);
 % optim param
 f = function_handle(f);
 iterations = 1000;
-alpha = 0.01;
-c_init = [rand(1) * 10000];
+alpha = 0.0001;
+c_init = [rand(1) * 5; rand(1) * 5]
 c_best = c_init;
-f_best = f(c_init);
+f_best = f(c_init(1), c_init(2))
 
 % pass gradient into solver function and return next point
 c_curr = c_init;
-disp("Starting iterations")
+disp("Starting iterations...\n")
 for iter = 1:iterations
 
-  c_next = steepest_descent_univariate(c_curr, df, alpha);
-  f_next = f(c_next);
+  c_next = steepest_descent_bivariate(c_curr, df, alpha);
+  f_next = f(c_next(1), c_next(2));
 
   if f_next < f_best
     c_best = c_next;
@@ -44,7 +44,7 @@ for iter = 1:iterations
 endfor
 disp("End of iterations...\n")
 
-% Best solution is c_best=[0,...,0] and f_best=0
+% Best solution is c_best=[1,...,1] and f_best=0
 disp("Best x:")
 disp(c_best)
 printf("Best f: %f\n", f_best)
--- src/solvers.m
+++ src/solvers.m
@@ -2,7 +2,6 @@
 1; % see https://docs.octave.org/interpreter/Script-Files.html
 function c_next = steepest_descent_univariate(c_curr, df, alpha)
 
-  % calculate direction at first point
   p_next = -df(c_curr);
   c_next = c_curr + alpha * p_next;
 
@@ -16,4 +15,18 @@ function c_next = quasi_newton_sr1_univariate(c_curr, df, alpha, B_curr)
 
  endfunction
 
+function c_next = steepest_descent_bivariate(c_curr, df, alpha)
+
+  p_next = -df(c_curr(1), c_curr(2));
+  c_next = c_curr + alpha * p_next;
+
+endfunction
+
+
+function c_next = quasi_newton_sr1_bivariate(c_curr, df, alpha, B_curr)
+
+  p_curr = -inv(B_curr) * df(c_curr(1), c_curr(2));
+  c_next = c_curr + alpha * p_curr
+
+ endfunction
 
